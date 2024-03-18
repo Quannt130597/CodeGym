efb64f6bf543387a950ea505899511eafd690d92
Quannt <quannt130597@gmail.com>
2024-03-18 20:31:13 +0700

    Bai tap 130324

--- 17-myfile.py
+++ 17-myfile.py
@@ -5,8 +5,13 @@ print ("File có đóng hoặc không?: ", file.close)
 print ("Chế độ mở file: ", file.mode)
 
 # Đọc file...........
-str = file.read()
-print ("Noi dung file la:\n",str)
+# str = file.read()
+# print ("Noi dung file la:\n",str)
+
+line1  = file.readline()
+line2 = file.readline()
+print ("dòng 1: ", line1)
+print ("dòng 2: ", line2)
 
 # Đóng sau khi xử lý file
 file.close ()
